0a2e37840b2e390e6212ddacd852e4bbdfc215b6
davidmilata <david.milata@devx.agency>
2024-04-17 19:47:17 +0200

    First commit init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vítejte Koblížcí
         </p>
         <a
           className="App-link"
